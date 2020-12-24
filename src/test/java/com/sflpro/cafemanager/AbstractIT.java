21452ca012c137de2582c74048bbda6ad5f0b880
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2020-12-25 02:03:08 +0330

    initial commit

---
+++ src/test/java/com/sflpro/cafemanager/AbstractIT.java
@@ -0,0 +1,30 @@
+package com.sflpro.cafemanager;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class AbstractIT {
+    @Container
+    public static MySQLContainer<?> database = new MySQLContainer<>(DockerImageName.parse("mysql:8"))
+            .withDatabaseName("test")
+            .withUsername("user")
+            .withPassword("pass");
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.password", database::getPassword);
+        registry.add("spring.datasource.username", database::getUsername);
+    }
+}
