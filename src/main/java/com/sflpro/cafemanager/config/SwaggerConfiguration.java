4d3e84f414c2cf8c0b7d22e2f3b2a5e8c0f51376
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2020-12-25 18:48:39 +0330

    feat: setup Swagger UI

---
+++ src/main/java/com/sflpro/cafemanager/config/SwaggerConfiguration.java
@@ -0,0 +1,21 @@
+package com.sflpro.cafemanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Component
+public class SwaggerConfiguration {
+
+    @Bean
+    private Docket docker() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sflpro.cafemanager"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
