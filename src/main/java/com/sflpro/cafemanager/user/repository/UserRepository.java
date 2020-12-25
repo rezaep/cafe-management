d40a691c65be3003e935ab369833fb1ba87d92b7
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2020-12-25 13:33:05 +0330

    feat: create UserRepository

---
+++ src/main/java/com/sflpro/cafemanager/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sflpro.cafemanager.user.repository;
+
+import com.sflpro.cafemanager.user.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsername(String name);
+}
