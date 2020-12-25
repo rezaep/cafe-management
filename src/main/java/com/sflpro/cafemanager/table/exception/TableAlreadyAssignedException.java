267900a2a5f2e8da64465df2250883eb336e5e67
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2020-12-25 18:38:18 +0330

    feat: assign table to user service

---
+++ src/main/java/com/sflpro/cafemanager/table/exception/TableAlreadyAssignedException.java
@@ -0,0 +1,5 @@
+package com.sflpro.cafemanager.table.exception;
+
+public class TableAlreadyAssignedException extends RuntimeException{
+
+}
--- src/main/java/com/sflpro/cafemanager/table/service/TableService.java
+++ src/main/java/com/sflpro/cafemanager/table/service/TableService.java
@@ -1,9 +1,12 @@
 package com.sflpro.cafemanager.table.service;
 
 import com.sflpro.cafemanager.exception.AlreadyExistException;
+import com.sflpro.cafemanager.exception.NotFoundException;
 import com.sflpro.cafemanager.table.domain.entity.Table;
 import com.sflpro.cafemanager.table.domain.model.TableModel;
+import com.sflpro.cafemanager.table.exception.TableAlreadyAssignedException;
 import com.sflpro.cafemanager.table.repository.TableRepository;
+import com.sflpro.cafemanager.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -24,6 +27,23 @@ public class TableService {
         tableRepository.save(table);
 
         return convertToModel(table);
+    }
+
+    private Table findTableById(long id) {
+        return tableRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Table not found"));
+    }
+
+    public void assignTableToUser(long tableId, User user) {
+        Table table = findTableById(tableId);
+
+        if (!ObjectUtils.isEmpty(table.getUser())) {
+            throw new TableAlreadyAssignedException();
+        }
+
+        table.setUser(user);
+
+        tableRepository.save(table);
     }
 
     private TableModel convertToModel(Table table) {
