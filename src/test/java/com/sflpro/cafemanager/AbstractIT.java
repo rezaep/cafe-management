b9ef26270167d71904b9ea24932e47af46c3f931
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2020-12-25 17:34:54 +0330

    feat: more spring configs

--- src/test/java/com/sflpro/cafemanager/AbstractIT.java
+++ src/test/java/com/sflpro/cafemanager/AbstractIT.java
@@ -1,7 +1,9 @@
 package com.sflpro.cafemanager;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
@@ -10,6 +12,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class AbstractIT {
