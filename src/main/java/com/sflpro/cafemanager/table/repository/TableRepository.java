045fb7b8f4d2ce28050f00b6503a35c8241b2d3d
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2020-12-25 19:22:03 +0330

    feat: get assigned tables to user

--- src/main/java/com/sflpro/cafemanager/table/repository/TableRepository.java
+++ src/main/java/com/sflpro/cafemanager/table/repository/TableRepository.java
@@ -2,9 +2,16 @@ package com.sflpro.cafemanager.table.repository;
 
 import com.sflpro.cafemanager.table.domain.entity.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TableRepository extends JpaRepository<Table, Long> {
     boolean existsByNumber(int number);
+
+    @Query("from table t where t.user.id=:userId")
+    List<Table> findAssignedTables(@Param("userId") long userId);
 }
