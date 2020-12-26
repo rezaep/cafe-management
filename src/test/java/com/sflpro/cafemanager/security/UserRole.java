f6999994419b39e7f991918da6ab0102b3a3a519
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2020-12-26 17:03:29 +0330

    build: create UserRole

---
+++ src/test/java/com/sflpro/cafemanager/security/UserRole.java
@@ -0,0 +1,6 @@
+package com.sflpro.cafemanager.security;
+
+public class UserRole {
+    public static final String MANAGER_ROLE = "MANAGER";
+    public static final String WAITER_ROLE = "WAITER";
+}
